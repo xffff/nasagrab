5bc5f6f7f0ea1af9dc5acc683daa2be682430c96
xffff <shouhei@gmail.com>
2013-10-21 20:09:36 +0100

    seems to work

--- bgchg.py
+++ bgchg.py
@@ -1,18 +1,22 @@
 from BeautifulSoup import BeautifulSoup
-import argparse, urllib2, sys, re, urlparse
+import argparse, urllib2, urllib, sys, re, urlparse, os
 
 def grabimg(url, imgpath):
     u = urllib2.urlopen(url)
     soup = BeautifulSoup(u)
     for i in soup.findAll('img', attrs={'src': re.compile('(?i)(jpg|png)$')}):
         imgurl = urlparse.urljoin(url, i['src'])
-        urllib2.urlretrieve(imgurl, imgpath)
-        print "Downloading: ", imgurl
+        name = i['src'].split("/")[-1]
+        if not os.path.isfile(imgpath + name):
+            urllib.urlretrieve(imgurl, imgpath + name)
+            print "Downloading: ", imgurl
+        else:
+            print "Image ", name, " already exists"
 
 def main():
     url = "http://apod.nasa.gov/apod/"
-    imgpath = "/Nasa"
-    grabimg(url)
+    imgpath = "/Nasa/"
+    grabimg(url,imgpath)
     return 0
 
 if __name__ == "__main__":
